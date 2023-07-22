ebddb322d5cbb0ffb7f345af6ecc862077c92fa4
Maneth-ii <manethbandara25@gmail.com>
2023-07-22 16:50:00 +0530

    bug fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/newsletter-sign-up-with-success-message-main-frontend-mentor-",
   plugins: [react()],
 })
