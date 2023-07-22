cf31e14d911441b892dc47bd86df407979209466
Maneth-ii <manethbandara25@gmail.com>
2023-07-22 19:48:23 +0530

    title change

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "/newsletter-sign-up-with-success-message-main-frontend-mentor-",
   plugins: [react()],
 })
